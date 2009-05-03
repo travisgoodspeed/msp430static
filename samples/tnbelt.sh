9424d4b4145fc45b4e18920b049b3dd36362096f
travisutk <travisutk@983cd025-1a47-0410-a2c2-905a3bf129a2>
2009-05-03 21:35:48 +0000

    Proper dump.
    git-svn-id: https://svn.code.sf.net/p/msp430static/code/trunk@80 983cd025-1a47-0410-a2c2-905a3bf129a2

--- samples/tnbelt.sh
+++ samples/tnbelt.sh
@@ -2,5 +2,5 @@
 echo "Erasing old database, if one exists."
 rm -f 430static.db
 echo "Disassembling and importing tnbelt.exe."
-msp430-objdump -d -m msp430 tnbelt.exe | m4s init
+msp430-objdump -D -m msp430 tnbelt.hex | m4s init
 echo "Done."
